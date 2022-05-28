78908dfe76da399cea58490be9cdeb9ef43a5d38
teranga@wiley.com <95670397+tharinduwiley@users.noreply.github.com>
2022-05-28 15:39:02 +0530

    add files to git

---
+++ src/main/java/com/microservice/demo/GatewayDemoApplication.java
@@ -0,0 +1,15 @@
+package com.microservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayDemoApplication.class, args);
+	}
+
+}
